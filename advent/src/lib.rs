f982212c409eb7054a8ee3ac5b67961a1dc01b28
RuiJCS <rjcsvv@gmail.com>
2020-12-30 21:56:33 +0000

    Added answers to the challenges of day 5 and 6

--- advent/src/lib.rs
+++ advent/src/lib.rs
@@ -2,7 +2,7 @@ pub mod advent {
 
     use itertools::Itertools;
     use regex::Regex;
-    use std::fs::File;
+    use std::{collections::BTreeMap, fs::File};
     use std::io::prelude::*;
     use std::{collections::HashMap, vec::Vec};
 
@@ -26,7 +26,7 @@ pub mod advent {
         input.lines().map(|s| s.to_string()).collect()
     }
 
-    pub fn twenty_twenty_two_entries(input: String) {
+    pub fn twenty_twenty_two_entries(input: String) -> u32 {
         let vals = read_string_to_vec_numbers(input);
         let res: Vec<i32> = vals
             .clone()
@@ -34,12 +34,11 @@ pub mod advent {
             .map(|x| 2020 - x)
             .filter(|x| vals.contains(x))
             .collect();
-        println!("{:?}", res);
-        println!("{:?}", res.into_iter().fold(1, |mul, x| mul * x));
+        res.into_iter().fold(1, |mul, x| mul * x)
     }
 
     /// This makes all the possible permutations, must find a smarter and more efficient way of doing this
-    pub fn twenty_twenty_three_entries(input: String) {
+    pub fn twenty_twenty_three_entries(input: String) -> u32 {
         let vals = read_string_to_vec_numbers(input);
         let res = &vals
             .clone()
@@ -48,12 +47,7 @@ pub mod advent {
             .unique()
             .filter(|val| val[0] + val[1] + val[2] == 2020)
             .collect::<Vec<Vec<i32>>>()[0];
-        println!(
-            "{:?} {} {}",
-            res,
-            res.iter().fold(0, |acc, x| acc + x),
-            res.iter().fold(1, |mul, x| mul * x)
-        );
+        res.iter().fold(1, |mul, x| mul * x
     }
 
     pub fn password_parsing_min_max(input: String) -> u32 {
@@ -117,13 +111,8 @@ pub mod advent {
                     u32::from_str_radix(y.to_string().as_str(), 10).unwrap(),
                     z.chars().nth(0).unwrap(),
                 );
-                println!("YOLO{} {} {} {}", pass, first, second, pass.len());
                 let x = pass.clone().chars().nth(first as usize).unwrap();
                 let y = pass.clone().chars().nth(second as usize).unwrap();
-                // let (x, y) = (
-                //     pass.chars().nth(first as usize).unwrap(),
-                //     pass.chars().nth(second as usize).unwrap(),
-                // );
                 x != y && (x == letter || y == letter)
             })
             .count();
@@ -136,7 +125,6 @@ pub mod advent {
             .filter(|s| !s.is_empty())
             .collect();
         let ys: Vec<usize> = (0..mountain.len()).step_by(velocity_y as usize).collect();
-        // mountain.iter().for_each(|s| println!("{}", *s));
         let (acc, _) = mountain
             .into_iter()
             .enumerate()
@@ -234,4 +222,184 @@ pub mod advent {
             .iter()
             .fold(0, |res, m| parse_passport(m) as u32 + res)
     }
+
+    pub fn parse_tickets(input: &String) -> Vec<(Vec<u32>, Vec<u32>)> {
+        input
+            .lines()
+            .map(|s| {
+                let parsed_string = s
+                    .to_string()
+                    .replace("F", "0")
+                    .replace("B", "1")
+                    .replace("L", "0")
+                    .replace("R", "1");
+                let (row, seat) = parsed_string.split_at(7);
+                (
+                    row.chars()
+                        .map(|c| c.to_digit(10).unwrap())
+                        .collect::<Vec<u32>>(),
+                    seat.chars()
+                        .map(|c| c.to_digit(10).unwrap())
+                        .collect::<Vec<u32>>(),
+                )
+            })
+            .collect::<Vec<(Vec<u32>, Vec<u32>)>>()
+    }
+
+    pub fn binary_to_decimal(base: &u32, exponent: &u32) -> u32 {
+        base * 2u32.pow(exponent - 1)
+    }
+
+    pub fn parse_tickets_to_map(input: &String) -> HashMap<Vec<u32>, Vec<Vec<u32>>> {
+        let tickets = input
+            .lines()
+            .map(|s| {
+                let parsed_string = s
+                    .to_string()
+                    .replace("F", "0")
+                    .replace("B", "1")
+                    .replace("L", "0")
+                    .replace("R", "1");
+                let (row, seat) = parsed_string.split_at(7);
+                (
+                    row.chars()
+                        .map(|c| c.to_digit(10).unwrap())
+                        .collect::<Vec<u32>>(),
+                    seat.chars()
+                        .map(|c| c.to_digit(10).unwrap())
+                        .collect::<Vec<u32>>(),
+                )
+            })
+            .collect::<Vec<(Vec<u32>, Vec<u32>)>>();
+        tickets.iter().fold(
+            HashMap::<Vec<u32>, Vec<Vec<u32>>>::new(),
+            |mut map, (row, seat)| {
+                let entry = map.entry(row.clone()).or_insert(vec![seat.clone()]);
+                if !entry.contains(&seat) {
+                    entry.push(seat.clone());
+                }
+                map
+            },
+        )
+    }
+
+    pub fn biggest_ticket(input: &String) -> u32 {
+        let tickets = parse_tickets(input);
+        tickets.iter().fold(0u32, |max, (row, seat)| {
+            let row_number = row
+                .iter()
+                .fold((0u32, 7u32), |(a, e), r| {
+                    (binary_to_decimal(r, &e) + a, e - 1)
+                })
+                .0
+                * 8u32;
+            let row_number = row_number
+                + seat
+                    .iter()
+                    .fold((0u32, 3u32), |(a, e), s| {
+                        (binary_to_decimal(s, &e) + a, e - 1)
+                    })
+                    .0;
+            max * (max >= row_number) as u32 + row_number * (max < row_number) as u32
+        })
+    }
+
+    pub fn my_seat(input: &String) -> u32 {
+        let tickets = parse_tickets_to_map(input);
+        let my_row = tickets
+            .into_iter()
+            .filter(|(k, v)| {
+                (*k.as_slice() != [0; 7] && *k.as_slice() != [1; 7]) && (*v).len() != 8
+            })
+            .collect::<HashMap<Vec<u32>, Vec<Vec<u32>>>>();
+        let seats: Vec<Vec<u32>> = vec![
+            vec![0, 1, 0],
+            vec![1, 0, 1],
+            vec![0, 1, 1],
+            vec![1, 1, 0],
+            vec![0, 0, 1],
+            vec![1, 0, 0],
+            vec![1, 1, 1],
+            vec![0, 0, 0],
+        ];
+        my_row
+            .iter()
+            .fold((0, seats), |(acc, seats), (k, v)| {
+                let seat_number = k
+                    .iter()
+                    .fold((0, 7), |(sum, exponent), n| {
+                        (sum + binary_to_decimal(n, &exponent), exponent - 1)
+                    })
+                    .0;
+                let seat: Vec<u32> = (seats
+                    .clone()
+                    .into_iter()
+                    .filter(|l| !v.contains(l))
+                    .collect::<Vec<Vec<u32>>>())[0]
+                    .clone();
+                let seat_number = seat_number * 8
+                    + seat
+                        .iter()
+                        .fold((0, 3), |(acc, exponent), i| {
+                            (acc + binary_to_decimal(i, &exponent), exponent - 1)
+                        })
+                        .0;
+                (acc + seat_number, seats)
+            })
+            .0
+    }
+
+    pub fn count_different_answers(input: &String) -> u32 {
+        input
+            .split("\n\n")
+            .collect::<Vec<&str>>()
+            .iter()
+            .fold(0, |acc, s| {
+                acc + s
+                    .to_string()
+                    .replace("\n", "")
+                    .chars()
+                    .sorted()
+                    .dedup()
+                    .collect_vec()
+                    .len() as u32
+            })
+    }
+
+    pub fn count_similar_answers(input: &String) -> u32 {
+        // Break up by group of answers
+        input.split("\n\n").collect_vec().iter().fold(0, |acc, s| {
+            //Separo por linha
+            let vec_string = s.split("\n").collect_vec();
+            let res = vec_string
+                .iter()
+                .fold(
+                    HashMap::<char, u32>::new(),
+                    |mut map: HashMap<char, u32>, s| {
+                        //count the charts ocurrence
+                        s.chars().for_each(|c| {
+                            let entry = map.entry(c).or_insert(0);
+                            *entry += 1;
+                        });
+                        map
+                    },
+                )
+                .into_iter()
+                .filter(|(key, value)| {
+                    // see if the occurrence of each character is the same as the number of lines
+                    *value >= vec_string.len() as u32
+                })
+                .count() as u32;
+            res + acc
+        })
+    }
+
+    // pub fn count_shinny(input: &String) -> u32 {
+    //     let rules = input.lines().collect_vec();
+    //     let regex = Regex::new(".*\\d\\s.*").unwrap();
+    //     let gold_bags = input.lines().filter(|s| regex.is_match(s));
+    //     let gold_allowed = rules.iter().fold(HashMap::<BTreeMap<(&str,u32)>::new(), |mut map|);
+
+    //     0
+    // }
 }
--- advent/src/main.rs
+++ advent/src/main.rs
@@ -3,15 +3,18 @@ const EXPENSES_FILE_NAME: &str = "files/expenses.txt";
 const PASSWORDS_FILE_NAME: &str = "files/passwords.txt";
 const MOUNTAIN_FILE_NAME: &str = "files/mountain.txt";
 const PASSPORTS_FILE_NAME: &str = "files/passports.txt";
+const TICKETS_FILE_NAME: &str = "files/tickets.txt";
+const ANSWERS_FILE_NAME: &str = "files/answers.txt";
+const RULES_FILE_NAME: &str = "files/rules.txt";
 const ERROR_MESSAGE: &str = "Error reading file";
 
 fn main() {
-    let file = advent::advent::read_file(PASSPORTS_FILE_NAME, ERROR_MESSAGE);
+    let file = advent::advent::read_file(ANSWERS_FILE_NAME, ERROR_MESSAGE);
     // let res = mountain_sliding(&file, 3, 1)
     //     * mountain_sliding(&file, 1, 1)
     //     * mountain_sliding(&file, 5, 1)
     //     * mountain_sliding(&file, 7, 1)
     //     * mountain_sliding(&file, 1, 2);
-    let res = valid_passports_rules(&file);
+    let res = count_similar_answers(&file);
     println!("{}", res);
 }
